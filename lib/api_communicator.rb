f5e2fa3dcf99d91083cc46743337d3ea668fc550
github username <github email address>
2018-11-06 18:50:18 +0000

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,22 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  character_data = character_hash["results"].find {|data| data["name"] === character}
+  characters_film_urls = character_data["films"]
+  film_names = characters_film_urls.map {|data| JSON.parse(RestClient.get(data))}
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  new_array = []
+  films_hash.each.with_index do |film, i|
+    puts "#{i + 1} " + film['title']
+  end
+  return new_array
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
